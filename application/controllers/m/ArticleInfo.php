0ed62dc2309e5394475812d0c835615febb3d1b4
455136018@qq.com <455136018@qq.com>
2019-01-07 15:29:58 +0800

    liwei

--- application/controllers/m/ArticleInfo.php
+++ application/controllers/m/ArticleInfo.php
@@ -60,60 +60,37 @@ class ArticleInfo extends CI_Controller {
         }
     }
 	
-	
-	 public function do_upload()
-    {
-        header("content-type:text/html;charset=utf-8");
-        $config['upload_path']      = './upload/recommend';
-        // 允许上传哪些类型
-        $config['allowed_types'] = 'gif|png|jpg|jpeg';
-        // 上传后的文件名，用uniqid()保证文件名唯一
-        echo $config['file_name'] = uniqid();
-        //var_dump($_FILES);
-        // 加载上传库
-		$config['max_size']=100;
-        $config['max_width']=1024;
-        $config['max_height']=768;
-        $this->load->library('upload', $config);
-        var_dump($this->upload->data());
-        // 上传文件，这里的pic是视图中file控件的name属性
-       // $result = $this->upload->do_upload('test');
-        $a = $this->upload->display_errors('<p>', '</p>');
-       var_dump($a);
-        // 如果上传成功，获取上传文件的信息
-        // if ($result)
-        // {
-            // var_dump($this->upload->data());
-        // }
-    }
-	
-	// public function article_add(){
-		// $arr = array();
-		// $title = trim($this->input->post('title'));
-		// $uid = trim($this->input->post('uid'));
-		
-		// $config['upload_path']='./uploads/';
-        // $config['allowed_types']='gif|jpg|png';
-        // $config['max_size']=100;
-        // $config['max_width']=1024;
-        // $config['max_height']=768;
 
-        // $this->load->library('upload',$config);
-        // if(!$this->upload->do_upload('userfile')){
-            // $error=array('error'=>$this->upload->display_errors());
-            // $this->load->view('test',$error);        
-        // }else{
-            // $data=array('upload_data'=>$this->upload->data());
-            // $this->load->view('upload_success',$data);
-        // }
 		
-		
-		
-		// $img = trim($this->input->post('img'));
-		// $publish_time = time();
-		// if($title =='' || $uid=='' || $img=='' || $publish_time==''){
-			// show_json($data,'缺少参数');
-            // exit;
-		// }
-	// }
+	public function article_add(){
+		$arr = array();
+		$title = trim($this->input->post('title'));
+		$uid = trim($this->input->post('uid'));
+		$img = trim($this->input->post('img'));
+		$publish_time = time();
+		if($title =='' || $uid=='' || $img=='' || $publish_time==''){
+			show_json($data,'缺少参数');
+			exit;
+		}
+		$result = $this->db->select('*')->where('uid',$uid)->get('blog_userinfo')->result();
+		if(empty($result)){
+			show_json($data,'用户不存在');
+			exit;
+		}
+		$data['title'] =  $title;
+		$data['uid'] =  $uid;
+		$data['img'] =  $img;
+		$data['publish_time'] =  $publish_time;
+		$this->db->insert('blog_article', $data);
+		$last_id = $this->db->insert_id();
+		 if($last_id>0){
+			$data["data"]=$arr;
+			$data["respcode"] = "000";
+			show_json($data,'发布文章成功');
+		}else{
+			$data["data"]=$arr;
+			$data["respcode"] = "401";
+			show_json($data,'发布文章失败');
+		}
+	}
 }
